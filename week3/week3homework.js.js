54d9faa89a3124d5adc055b4e9c473f2d2fcab4c
tanyavszabo <tatianavszabo@gmail.com>
2023-09-18 14:50:18 -0400

    week 3 homwework

--- week3/week3homework.js.js
+++ week3/week3homework.js.js
@@ -1,39 +1,48 @@
-//Create an array of pizzaToppings with at least four different toppings
+
 Let PizzaToppings = ["Pepperoni","Cheese","Bacon","Pineapple","Olives"];
 
+function greetCustomer() {
+  console.log(
+  "Welcome to Pizza House! Our toppings tonight are:");
+for (let name of PizzaToppings)
+{
+  console.log(name)
+}
+}
+greetCustomer();
+function getPizzaOrder(size, crust,...toppings) {
+  console.log(`One ${size} ${crust} pizza with ${toppings} coming right up!`);
+  return [size, crust, toppings];
 
-//2. Create a greetCustomer function that prints a message that welcomes a guest, then informs them of the available toppings by looping over pizzaToppings (don't worry about perfect grammar here yet, i.e. "a, b, and c", see Bonus Challenge #9)
-//i.e. "Welcome to Pizza House, our toppings are: a, b, c, ..."
+}
+getPizzaOrder("large","thick", PizzaToppings);
 
-function greetCustomer(){
-  let greeting =
-  console.log(`Welcome to Pizza House, our toppings are ${PizzaToppings} `);
-//3. Create a getPizzaOrder function that
-//has the parameters size, crust, and an indefinite amount of toppings as inputs
-//prints the order, i.e. "One large thick crust pizza with x, y, z, ... coming up!"
-//outputs a list with the size, crust, and toppings
+function preparePizza([size, crust, pizzaToppings])
 
 function getPizzaOrder(Size, Crust, ...PizzaToppings);{
-  console.log("One" Size, Crust,"Crusted pizza with" PizzaToppings ..."Coming right up!");
+  console.log('One ${Size} ${crust} right away!`);
+  return [size, crust, toppings];
 }
-getPizzaOrder("Large","Thick", pepperoni);
-//4. Create a preparePizza function that
-//has an array as its parameter with three items: a size, a crust, and a list of toppings
-//prints something like "...Cooking pizza..."
-//outputs a pizza Object with appropriate key-value pairs for size, crust, and toppings
-let preparePizza = ["Large","Thick",PizzaToppings]
-//5. Create a servePizza function that
-//has a parameter of a pizza Object
-let servePizza = 
-//logs a message that the pizza is ready and repeats the order, i.e. "Order up! Here's your large thick crust pizza with x, y, z, ... Enjoy!"
-//outputs the same pizza Object that was passed in
 
-//6.Call each function and (starting with preparePizza) use the returned value from the previous function as its input
-//Add, commit, and push your JS file to your GitHub repo.
-//Send your TA a link to your GitHub Homework repo when finished.
+function preparePizza([size, crust, pizzaToppings]) {
+  console.log(..."Baking your pizza...");
+  const pizzaType = {
+    size: size,
+    crust: crust,
+    toppings; pizzaToppings
+  };
+  return pizzaType;
+}
 
-An example output for this assignment is:
+let orderType = preparePizza(getPizzaOrder("large", "thick", pizzaToppings));
 
-Welcome to The Pizza House, our toppings are: pepperoni, sausage, onions, peppers, One large thin crust pizza with sausage, onions, peppers, . Coming up! ...your pizza is cooking... Order up! Here's your large thin crust pizza with sausage, onions, peppers, . Enjoy!
+function servePizza(pizzaType) {
+  console.log(
+    `Order is Hot and Ready! Enjoy your ${pizzaType.size} ${pizzaType.crust} with ${pizzaType.toppings}`);
+    return pizzaType;
+}
+servePizza(orderType)
+
+
 
 
