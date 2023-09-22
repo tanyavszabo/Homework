1b29bb390dd4a96342b2f0f8acafc68cd07f8cf7
tanyavszabo <tatianavszabo@gmail.com>
2023-09-22 13:01:36 -0400

    Week4Homework

--- week3/week3homework.js
+++ week3/week3homework.js
@@ -10,16 +10,18 @@ function greetCustomer() {
 greetCustomer();
 
 function getPizzaOrder(size, crust, ...toppings) {
-  let order = `One ${size} ${crust} pizza with` 
-  for(let topping of toppings) {
+  console.log(toppings);
+
+  let order = `One ${size} ${crust} pizza with`;
+  for (let topping of toppings) {
     order += ` ${topping}`;
   }
- order += " coming right up!";
-//console.log(order);
+  order += " coming right up!";
+  console.log(order);
 
   return [size, crust, toppings];
 }
-console.log (getPizzaOrder("large", "thick", pizzaToppings));
+console.log(getPizzaOrder("large", "thick", "pepperoni", "mushrooms"));
 
 function preparePizza(size, crust, toppings) {
   console.log("...Cooking pizza...");
@@ -30,11 +32,16 @@ function preparePizza(size, crust, toppings) {
   };
   return pizzaType;
 }
-let orderType = preparePizza(getPizzaOrder("large", "thick", pizzaToppings));
+let orderType = preparePizza(
+  getPizzaOrder("large", "thick", "pepperoni", "mushrooms")
+);
 
 function servePizza(pizzaType) {
-  console.log(`Order is Hot and Ready!! Here's your ${pizzaType.size} ${pizzaType.crust} with ${pizzaType.toppings}. Enjoy your Pizza!`
+  console.log(
+    `Order is Hot and Ready!! Here's your ${pizzaType.size} ${pizzaType.crust} with ${pizzaType.toppings}. Enjoy your Pizza!`
   );
+
   return pizzaType;
 }
 servePizza(orderType);
+// output pizza type. toppings into loop exactly like first two functions.
